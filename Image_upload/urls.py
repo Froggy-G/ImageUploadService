61794a4a3007e77b956c278421f485f26b14177b
Froggy-G <pwe756@yandex.ru>
2021-07-10 23:05:33 +0400

    make a personal page for every picture

--- Image_upload/urls.py
+++ Image_upload/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.image_list, name='Image_list')
+    path('', views.image_list, name='image_list'),
+    path('image/<int:pk>', views.image_page, name='image_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Image_upload/views.py
+++ Image_upload/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Image
-from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 def image_list(request):
     images = Image.objects.all()
-    return render(request, 'Image_upload/image_list.html', {'images': images})
+    context = {'images': images}
+    return render(request, 'Image_upload/image_list.html', context)
+
+def image_page(request, pk):
+    image = get_object_or_404(Image, pk=pk)
+    context = {'image': image}
+    return render(request, 'Image_upload/image_page.html', context)
