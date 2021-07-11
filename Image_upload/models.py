ca726734e358cdc03807917c987cd06387800470
Froggy-G <pwe756@yandex.ru>
2021-07-11 16:57:14 +0400

    feat views counter

--- Image_upload/models.py
+++ Image_upload/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images')
+    views = models.PositiveIntegerField(default=0)
     
     def __str__(self):
         return self.name
--- Image_upload/views.py
+++ Image_upload/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Image
 from django.shortcuts import get_object_or_404
+from django.db.models import F
 
 def image_list(request):
     images = Image.objects.all()
@@ -9,5 +10,6 @@ def image_list(request):
 
 def image_page(request, pk):
     image = get_object_or_404(Image, pk=pk)
+    Image.objects.filter(pk=image.pk).update(views=F('views') + 1)
     context = {'image': image}
     return render(request, 'Image_upload/image_page.html', context)
