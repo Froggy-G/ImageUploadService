101638f894ad27a533477d393dfabd7b8355789b
Froggy-G <pwe756@yandex.ru>
2021-07-11 17:55:04 +0400

    feat image upload page

--- Image_upload/urls.py
+++ Image_upload/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('', views.image_list, name='image_list'),
     path('image/<int:pk>', views.image_page, name='image_page'),
+    path('image/new', views.image_new, name='image_new')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Image_upload/views.py
+++ Image_upload/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Image
 from django.shortcuts import get_object_or_404
 from django.db.models import F
+from .forms import ImageUploadForm
 
 def image_list(request):
     images = Image.objects.all()
@@ -13,3 +14,14 @@ def image_page(request, pk):
     Image.objects.filter(pk=image.pk).update(views=F('views') + 1)
     context = {'image': image}
     return render(request, 'Image_upload/image_page.html', context)
+
+def image_new(request):
+    if request.method == "POST":
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('image_page', pk=post.pk)
+    else:
+        form = ImageUploadForm()
+    return render(request, 'Image_upload/image_new.html', {'form': form})
